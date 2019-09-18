b076a99d0061ded2695c27cc0271abb2629050c6
Reid Mitchell <reidmitchell1@gmail.com>
2019-09-17 21:38:08 -0700

    Simplifying module structure of tests package

--- tests/src/macros.rs
+++ tests/src/macros.rs
@@ -1,18 +1,4 @@
 #[macro_export]
-#[cfg(debug_assertions)]
-macro_rules! debug {
-  ($($arg:tt)*) => {{
-    println!($($arg)*);
-  }};
-}
-
-#[macro_export]
-#[cfg(not(debug_assertions))]
-macro_rules! debug {
-  ($($arg:tt)*) => {};
-}
-
-#[macro_export]
 macro_rules! assert_tokens_snapshot {
   ($name: ident, $source: literal) => {
     #[test]
