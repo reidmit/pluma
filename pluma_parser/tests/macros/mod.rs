b795435cb135c68efa154876b9b1b2e2b122dcec
Reid Mitchell <reidmitchell1@gmail.com>
2020-06-23 19:18:45 -0700

    Refactor unit tests to have macros in separate files

---
+++ pluma_parser/tests/macros/mod.rs
@@ -0,0 +1,40 @@
+#[macro_export]
+macro_rules! test_parse_success {
+  ($($name:ident: $source:literal,)*) => {
+    $(
+        #[test]
+        fn $name() {
+            use insta::assert_snapshot;
+            use pluma_parser::*;
+            use std::collections::HashMap;
+
+            let replaced = $source.replace("\n    |", "\n");
+            let source = replaced.trim();
+            let source_copy = source.clone();
+            let bytes = Vec::from(source);
+            let tokenizer = Tokenizer::from_source(&bytes);
+            let mut parser = Parser::new(&bytes, tokenizer);
+            let (ast, _imports, errors) = parser.parse_module();
+
+            if !errors.is_empty() {
+              panic!("parse errors: {:#?}", errors);
+            }
+
+            let file_name = format!("{}", stringify!($name));
+
+            let formatted = format!("
+=== Source ===
+{}
+
+=== Comments ===
+{:#?}
+
+=== AST ===
+{:#?}
+", source_copy, HashMap::<(), ()>::new(), ast);
+
+            assert_snapshot!(file_name, formatted, &source_copy);
+        }
+    )*
+  }
+}
--- pluma_parser/tests/parse_success.rs
+++ pluma_parser/tests/parse_success.rs
@@ -1,42 +1,5 @@
-use insta::assert_snapshot;
-use pluma_parser::*;
-use std::collections::HashMap;
-
-macro_rules! test_parse_success {
-  ($($name:ident: $source:literal,)*) => {
-    $(
-        #[test]
-        fn $name() {
-            let replaced = $source.replace("\n    |", "\n");
-            let source = replaced.trim();
-            let source_copy = source.clone();
-            let bytes = Vec::from(source);
-            let tokenizer = Tokenizer::from_source(&bytes);
-            let mut parser = Parser::new(&bytes, tokenizer);
-            let (ast, _imports, errors) = parser.parse_module();
-
-            if !errors.is_empty() {
-              panic!("parse errors: {:#?}", errors);
-            }
-
-            let file_name = format!("{}", stringify!($name));
-
-            let formatted = format!("
-=== Source ===
-{}
-
-=== Comments ===
-{:#?}
-
-=== AST ===
-{:#?}
-", source_copy, HashMap::<(), ()>::new(), ast);
-
-            assert_snapshot!(file_name, formatted, &source_copy);
-        }
-    )*
-  }
-}
+#[macro_use]
+mod macros;
 
 test_parse_success! {
   empty: r#"
