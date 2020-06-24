566f6d8be72f14212f07605aef882808ff550a81
Reid Mitchell <reidmitchell1@gmail.com>
2020-06-23 19:26:50 -0700

    Add some more analyzer tests

--- pluma_analyzer/tests/analyze_tests.rs
+++ pluma_analyzer/tests/analyze_tests.rs
@@ -8,6 +8,21 @@ test_analyze! {
 
   defined_identifier_simple (true): r#"
     |let fine = 47
+    |
     |fine
+  "#,
+
+  intrinsic_def_binary_op_valid (true): r#"
+    |intrinsic_type Int
+    |intrinsic_def Int + Int -> Int
+    |
+    |let result = 3 + 4
+  "#,
+
+  intrinsic_def_binary_op_invalid (false): r#"
+    |intrinsic_type Int
+    |intrinsic_def Int + Int -> Int
+    |
+    |let result = 3 + "yikes"
   "#,
 }
