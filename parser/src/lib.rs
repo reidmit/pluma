4ef506fdd74c154245450ed06825d0a789b57053
Reid Mitchell <6752783+reidmit@users.noreply.github.com>
2022-04-03 23:18:37 -0700

    Add initial parser snapshot test

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -4,6 +4,8 @@ mod tokenizer;
 mod tokens;
 
 #[cfg(test)]
+mod parser_test;
+#[cfg(test)]
 mod tokenizer_test;
 
 pub use self::parser::*;
---
+++ parser/src/parser_test.rs
@@ -0,0 +1,24 @@
+use crate::{Parser, Tokenizer};
+
+/// These tests cover the basic functionality of the parser.
+///
+/// See the snapshots/ directory in this crate for the snapshot output. The files tend
+/// to be named like `{crate}__{file}__{test_name}.snap` - for example, see
+/// `parser__parser_test__hello_world.snap`.
+
+macro_rules! snapshot {
+	($test_name:ident, $raw_source:literal) => {
+		#[test]
+		fn $test_name() {
+			let source = $raw_source;
+			let bytes = source.as_bytes().to_vec();
+			let mut tokenizer = Tokenizer::from_source(&bytes);
+			let mut parser = Parser::new(&bytes, tokenizer);
+			let result = parser.parse_module();
+
+			insta::assert_snapshot!(format!("{}\n\n{:#?}", source, result));
+		}
+	};
+}
+
+snapshot!(hello_world, "hello world");
