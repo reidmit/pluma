46fd709167f6c590a215c8a87f7bad715ad59d26
Reid Mitchell <6752783+reidmit@users.noreply.github.com>
2022-05-21 10:30:29 -0700

    Pretty-print lambdas

--- src/ast/lambda.rs
+++ src/ast/lambda.rs
@@ -1,15 +1,30 @@
 use super::*;
 use crate::expr_type::*;
 
-#[cfg_attr(debug_assertions, derive(Debug))]
 pub struct LambdaNode {
 	pub span: Span,
 	pub params: Vec<LambdaParamNode>,
 	pub body: Vec<ExprNode>,
 }
 
-#[cfg_attr(debug_assertions, derive(Debug))]
 pub struct LambdaParamNode {
 	pub ident: IdentifierNode,
 	pub inferred_type: ExprType,
+}
+
+#[cfg(debug_assertions)]
+impl std::fmt::Debug for LambdaNode {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.debug_struct(&format!("lambda({}-{})", self.span.0, self.span.1,))
+			.field("params", &self.params)
+			.field("body", &self.body)
+			.finish()
+	}
+}
+
+#[cfg(debug_assertions)]
+impl std::fmt::Debug for LambdaParamNode {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:#?} :: {}", self.ident, self.inferred_type)
+	}
 }
